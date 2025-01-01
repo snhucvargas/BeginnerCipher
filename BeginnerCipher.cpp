8f78526ccf37e0951a3a1b2c14d531b98bebe18c
Christopher Vargas <christopher.vargas2@snhu.edu>
2024-12-31 21:58:47 -0800

    Replaced cin input with getline() to keep whitespace. Added for loop to iterate through input string. For loop now contains a check for exceeding the end of the alphabet.

--- BeginnerCipher.cpp
+++ BeginnerCipher.cpp
@@ -7,23 +7,27 @@
 
 int main()
 {
+    // initialize variables
+    std::string input = "";
+    int cipherStep = 1;
+
     // Prompt the user for a message to encode.
     std::cout << "Please enter the message you would like to have encoded. Only letters will be encoded.\n";
-    std::string input = "";
-    std::cin >> input;
+    std::getline(std::cin, input);
 
     // Standardize input into all upper case letters.
     std::transform(input.begin(), input.end(), input.begin(), ::toupper);
+
+    // Iterate through the string
+    for (int i = 0; i < input.size(); i++) {
+        char encryptLetter = input[i] + cipherStep;
+
+        // Check if encrypted letter is outside of the boundary
+        if (encryptLetter > 'Z') {
+            // Loop back around the start of the alphabet
+            encryptLetter = encryptLetter - 'Z' + ('A' - 1);
+        }
+        
+    }
     
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
