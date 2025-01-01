853746f6a1c833d706574298e666f7b698640452
Christopher Vargas <christopher.vargas2@snhu.edu>
2024-12-31 22:16:43 -0800

    Removed letter-only restriction. Added a check to see if a character is a letter. Added a check to see if a letter is upper or lowercase. Added a console print for the encoded message.

--- BeginnerCipher.cpp
+++ BeginnerCipher.cpp
@@ -10,24 +10,42 @@ int main()
     // initialize variables
     std::string input = "";
     int cipherStep = 1;
+    int alphaStart = 65;
+    int alphaEnd = 122;
 
     // Prompt the user for a message to encode.
-    std::cout << "Please enter the message you would like to have encoded. Only letters will be encoded.\n";
+    std::cout << "Please enter the message you would like to have encoded.\n";
     std::getline(std::cin, input);
-
-    // Standardize input into all upper case letters.
-    std::transform(input.begin(), input.end(), input.begin(), ::toupper);
 
     // Iterate through the string
     for (int i = 0; i < input.size(); i++) {
-        char encryptLetter = input[i] + cipherStep;
+        // If the character is not a letter, leave as-is
+        if ((int)input[i] < alphaStart || (int)input[i] > alphaEnd) {
+            continue;
+        }
+        else {
+            char encryptLetter = input[i] + cipherStep;
 
-        // Check if encrypted letter is outside of the boundary
-        if (encryptLetter > 'Z') {
-            // Loop back around the start of the alphabet
-            encryptLetter = encryptLetter - 'Z' + ('A' - 1);
+            // Check if encrypted letter is outside of the boundary
+            // for lowercase
+            if (encryptLetter > 'a') {
+                if (encryptLetter > 'z') {
+                    encryptLetter = encryptLetter - 'z' + ('a' - 1);
+                }
+            }
+            // for uppercase
+            else if (encryptLetter > 'A') {
+                if (encryptLetter > 'Z') {
+                    // Loop back around the start of the alphabet
+                    encryptLetter = encryptLetter - 'Z' + ('A' - 1);
+                }
+            }
+
+            input[i] = encryptLetter;
         }
         
     }
-    
+
+    // Print out the encrypted message.
+    std::cout << input;    
 }
