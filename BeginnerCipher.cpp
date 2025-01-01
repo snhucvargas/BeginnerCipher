5c1605188a12cf3c34d748325ace37085d43bd2d
Christopher Vargas <christopher.vargas2@snhu.edu>
2024-12-31 22:25:31 -0800

    Added TODO items regarding cleaning up the main code body. Added declaration for encodeMessage and decodeMessage functions.

--- BeginnerCipher.cpp
+++ BeginnerCipher.cpp
@@ -5,6 +5,16 @@
 #include <algorithm>
 #include <string>
 
+// TODO: Write an encoder and decoder function
+
+std::string encodeMessage(std::string message, int cipherStep) {
+    return "Finish encoder function.";
+}
+
+std::string decodeMessage(std::string codedMessage, int cipherStep) {
+    return "Finish decoder function.";
+}
+
 int main()
 {
     // initialize variables
@@ -13,6 +23,12 @@ int main()
     int alphaStart = 65;
     int alphaEnd = 122;
 
+    // TODO: Add a prompt for the user to select whether they want to
+    // encode or decode a message.
+
+    // TODO: Run a while loop for repeated use
+
+    // For encoding
     // Prompt the user for a message to encode.
     std::cout << "Please enter the message you would like to have encoded.\n";
     std::getline(std::cin, input);
@@ -47,5 +63,10 @@ int main()
     }
 
     // Print out the encrypted message.
+    std::cout << "Here is your encoded message:\n";
     std::cout << input;    
+
+    // For decoding
+    // std::cout << "Please enter a message you would like to have decoded.\n"
+    // std::getline(std::cin, input);
 }
